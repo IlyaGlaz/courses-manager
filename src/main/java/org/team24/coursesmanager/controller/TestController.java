661b28e41bba978489c01ba194e789f81679a91b
Pavel Bal <pbal00@mail.ru>
2023-02-24 22:45:55 +0300

    feat: add swagger annotations in TestController

--- src/main/java/org/team24/coursesmanager/controller/TestController.java
+++ src/main/java/org/team24/coursesmanager/controller/TestController.java
@@ -1,13 +1,18 @@
 package org.team24.coursesmanager.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@Api(value = "Тестовый контроллер")
 public class TestController {
 
+    @ApiOperation(value = "Краткое описание эндпоинта",
+            notes = "Более полное описаение")
     @GetMapping("/test")
     public String test() {
         return "test";
