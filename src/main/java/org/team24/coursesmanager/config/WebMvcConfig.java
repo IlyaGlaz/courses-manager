9c945762b8cfd0af5fa5519daa2a0daed792a4c5
Ilya Glaz <glazterix@gmail.com>
2023-02-27 18:20:45 +0300

    @Component -> @Configuration

--- src/main/java/org/team24/coursesmanager/config/WebMvcConfig.java
+++ src/main/java/org/team24/coursesmanager/config/WebMvcConfig.java
@@ -1,11 +1,11 @@
 package org.team24.coursesmanager.config;
 
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Component
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
