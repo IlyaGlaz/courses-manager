d0949de10231ab6e5447b9ca8f64b205e8c41c58
unknown <Sergey-Bernikov@mail.ru>
2023-03-02 11:20:17 +0300

    removed DAO

--- src/main/java/org/team24/coursesmanager/dao/UserDAO.java
+++
@@ -1,8 +0,0 @@
-package org.team24.coursesmanager.dao;
-
-import org.team24.coursesmanager.entity.User;
-import java.util.List;
-public interface UserDAO {
-    List<User> getAllStudent();
-    List<User> getAllTeacher();
-}
--- src/main/java/org/team24/coursesmanager/service/impl/UserServiceImpl.java
+++ src/main/java/org/team24/coursesmanager/service/impl/UserServiceImpl.java
@@ -3,21 +3,21 @@ package org.team24.coursesmanager.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.team24.coursesmanager.dao.impl.UserDAOImpl;
 import org.team24.coursesmanager.entity.User;
+import org.team24.coursesmanager.repository.UserRepository;
 import org.team24.coursesmanager.service.UserService;
 import java.util.List;
 @Transactional
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired
-    UserDAOImpl userDAOImpl;
+    UserRepository userRepository;
     @Override
     public List<User> getAllStudent() {
-        return userDAOImpl.getAllStudent();
+        return userRepository.findRoleStudent();
     }
     @Override
     public List<User> getAllTeacher() {
-        return userDAOImpl.getAllTeacher();
+        return userRepository.findRoleTeacher();
     }
 }
