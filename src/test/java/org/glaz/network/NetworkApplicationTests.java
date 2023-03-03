d74bc54e419c4798734d87a9cd22d4715519facd
Илья Глаз <glazterix@gmail.com>
2023-03-03 17:54:02 +0300

    tastcontainers added

---
+++ src/test/java/org/glaz/network/NetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.glaz.network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
