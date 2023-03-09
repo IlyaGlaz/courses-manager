f824e72e31284952a18624be407f78f0d98949d5
SergeyBernikov <sergey-bernikov@mail.ru>
2023-03-10 00:29:11 +0300

    added random search for students

--- src/main/java/org/team24/coursesmanager/repository/UserRepository.java
+++ src/main/java/org/team24/coursesmanager/repository/UserRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.team24.coursesmanager.entity.User;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -14,7 +13,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findRoleStudent();
     @Query(value = "FROM User where role = 'TEACHER'")
     List<User> findRoleTeacher();
-    @Query(value = "FROM User where groups_id = :id")
+    @Query(value = "select * from users inner join users_xref_groups on users.id = users_xref_groups.users_id where group_id = :id and role = 'STUDENT_ROLE'", nativeQuery = true)
     List<User> listGroup(Long id);
     Optional<User> findByEmail(String email);
 }
