db28370d2de9e432ffb4a1d6cbca50bcf4865f1c
Pavel Bal <pbal00@mail.ru>
2023-03-09 23:05:48 +0300

    feat: add AuthController

---
+++ src/main/java/org/team24/coursesmanager/controller/AuthController.java
@@ -0,0 +1,90 @@
+package org.team24.coursesmanager.controller;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.team24.coursesmanager.dto.AuthenticationDTO;
+import org.team24.coursesmanager.dto.UserDto;
+import org.team24.coursesmanager.entity.User;
+import org.team24.coursesmanager.security.JWTUtil;
+import org.team24.coursesmanager.services.RegistrationService;
+import org.team24.coursesmanager.util.UserValidator;
+
+import javax.validation.Valid;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private final RegistrationService registrationService;
+    private final UserValidator userValidator;
+    private final JWTUtil jwtUtil;
+    private final ModelMapper modelMapper;
+
+    private final AuthenticationManager authenticationManager;
+
+
+    @Autowired
+    public AuthController(RegistrationService registrationService, UserValidator userValidator,
+                          JWTUtil jwtUtil, ModelMapper modelMapper, AuthenticationManager authenticationManager) {
+        this.registrationService = registrationService;
+        this.userValidator = userValidator;
+        this.jwtUtil = jwtUtil;
+        this.modelMapper = modelMapper;
+        this.authenticationManager = authenticationManager;
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "auth/login";
+    }
+
+    @GetMapping("/registration")
+    public String registrationPage(@ModelAttribute("user") User user) {
+        return "auth/registration";
+    }
+
+    @PostMapping("/registration")
+    public Map<String, String> performRegistration(@RequestBody @Valid UserDto UserDto,
+                                                   BindingResult bindingResult) {
+        User user = convertToUser(UserDto);
+
+        userValidator.validate(user, bindingResult);
+
+        if (bindingResult.hasErrors())
+            return Map.of("message", "Ошибка!");
+
+        registrationService.register(user);
+
+        String token = jwtUtil.generateToken(user.getUsername());
+        return Map.of("jwt-token", token);
+    }
+
+    @PostMapping("/login")
+    public Map<String, String> perfromLogin(@RequestBody AuthenticationDTO authenticationDTO) {
+        UsernamePasswordAuthenticationToken authInputToken =
+                new UsernamePasswordAuthenticationToken(authenticationDTO.getUsername(),authenticationDTO.getPassword());
+        try {
+            authenticationManager.authenticate(authInputToken);
+        } catch (BadCredentialsException e) {
+            return Map.of("message", "Incorrect credentionals");
+        }
+        String token = jwtUtil.generateToken(authenticationDTO.getUsername());
+        return Map.of("jwt-token", token);
+    }
+
+
+    public User convertToUser(UserDto UserDto) {
+        return this.modelMapper.map(UserDto, User.class);
+    }
+
+}
