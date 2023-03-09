045b0f53cfb5d151c46fa9e4b0a83f8a3177442c
Pavel Bal <pbal00@mail.ru>
2023-03-09 23:03:42 +0300

    feat: add RegistrationService

---
+++ src/main/java/org/team24/coursesmanager/services/RegistrationService.java
@@ -0,0 +1,29 @@
+package org.team24.coursesmanager.services;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.team24.coursesmanager.entity.User;
+import org.team24.coursesmanager.repositiries.UserRepository;
+
+@Service
+public class RegistrationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public RegistrationService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    public void register(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole("ROLE_STUDENT");
+        userRepository.save(user);
+    }
+}
