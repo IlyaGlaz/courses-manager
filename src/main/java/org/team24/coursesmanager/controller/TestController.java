e8887c91d24957b87845089f6b9daf6f4816eec4
Pavel Bal <pbal00@mail.ru>
2023-03-09 23:06:06 +0300

    feat: edit TestController

--- src/main/java/org/team24/coursesmanager/controller/TestController.java
+++ src/main/java/org/team24/coursesmanager/controller/TestController.java
@@ -4,15 +4,19 @@ import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.team24.coursesmanager.dto.GroupDto;
 import org.team24.coursesmanager.dto.UserDto;
 import org.team24.coursesmanager.dto.UserReadDto;
 import org.team24.coursesmanager.entity.Group;
 import org.team24.coursesmanager.entity.User;
+import org.team24.coursesmanager.security.PersonDetails;
 import org.team24.coursesmanager.service.GroupService;
 import org.team24.coursesmanager.service.UserService;
 
@@ -46,7 +50,22 @@ public class TestController {
     private UserDto convertToDto(User user) {
         return modelMapper.map(user, UserDto.class);
     }
+
     private GroupDto convertToDto(Group group) {
         return modelMapper.map(group, GroupDto.class);
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "admin";
+    }
+
+    @GetMapping("/showUserInfo")
+    @ResponseBody
+    public String showUserInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+
+        return personDetails.getUsername();
     }
 }
