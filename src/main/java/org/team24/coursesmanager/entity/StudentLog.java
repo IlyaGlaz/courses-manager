19300d65fe0211951ae48e9ebc24a0f947c44823
Pavel Bal <pbal00@mail.ru>
2023-03-09 19:24:26 +0300

    refactor: StudentLog refactor

--- src/main/java/org/team24/coursesmanager/entity/StudentLog.java
+++ src/main/java/org/team24/coursesmanager/entity/StudentLog.java
@@ -1,16 +1,21 @@
 package org.team24.coursesmanager.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "student_logs")
 public class StudentLog {
@@ -18,10 +23,7 @@ public class StudentLog {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
     private String text;
-
     private LocalDateTime publishingTime;
-
     private LocalDateTime editingTime;
 }
