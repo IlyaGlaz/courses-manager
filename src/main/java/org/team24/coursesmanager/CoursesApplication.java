fe8cd4c2923a712ac9542f7d0c3d4bb417438db2
Pavel Bal <pbal00@mail.ru>
2023-03-10 17:27:10 +0300

    feat: enable WebMvc for swagger

--- src/main/java/org/team24/coursesmanager/CoursesApplication.java
+++ src/main/java/org/team24/coursesmanager/CoursesApplication.java
@@ -2,6 +2,9 @@ package org.team24.coursesmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc           //For the Swagger to work
 @SpringBootApplication
 public class CoursesApplication {
     public static void main(String[] args) {
